180e78ee67ee03f07d75ca41d3d903bdc18bf6d2
jasonfritsche <fritsche.jason@gmail.com>
2021-07-24 16:17:49 -0500

    created list item component

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,16 +1,18 @@
 <template>
-  <ul class="max-w-sm mx-auto flex-col items-center justify-center">
-    <li class="text-center" v-for="(item, index) in list" :key="index">
-      {{ item }}
+  <ul class="grid grid-cols-1 justify-items-center">
+    <li class="w-2/3 h-10 my-1" v-for="(item, index) in list" :key="index">
+      <list-item :item="item" />
     </li>
   </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
+import ListItem from "./ListItem.vue";
 export default defineComponent({
+  components: { ListItem },
   setup() {
-    const list = ref(["item one", "item two", "item three"]);
+    const list = ref<Array<string>>(["item one", "item two", "item three"]);
 
     return {
       list: list,
