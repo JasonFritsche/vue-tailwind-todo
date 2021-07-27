8b33ee7c3a520a2149efe3e36707488c285984f0
jasonfritsche <fritsche.jason@gmail.com>
2021-07-26 21:07:57 -0500

    added validation of todo item and toast message

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -13,15 +13,29 @@
 import { defineComponent, ref } from "vue";
 import ListItem from "../List/ListItem.vue";
 import AddItem from "./AddItem.vue";
-import { ITodo } from "../../models/Todo";
+import { ITodo, TodoResponse } from "../../models/Todo";
+import { useToast } from "vue-toastification";
+
 export default defineComponent({
   components: { ListItem, AddItem },
   setup() {
+    const toast = useToast();
     const list = ref<Array<ITodo>>([]);
-    const addTodo = (todo: ITodo) => list.value.push(todo);
+    // const addTodo = (todo: ITodo) => list.value.push(todo);
+    const addTodo = (todo: TodoResponse) => {
+      if (todo.isValid) {
+        list.value.push(todo);
+        toast.success(todo.message, {
+          timeout: 2000,
+        });
+      } else if (!todo.isValid) {
+        toast.error(todo.message), { timeout: 2000 };
+      }
+    };
     return {
       list,
       addTodo,
+      toast,
     };
   },
 });
